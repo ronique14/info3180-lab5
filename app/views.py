13240fb416c6ea9786ca11020635ddc0c06f54f6
Ronique Reid <roni.reid.roni@gmail.com>
2022-03-06 21:47:31 -0500

    Code up to step 8

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -34,8 +35,13 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
+            username = form.username.data
+            password = form.password.data
+            
+            user = UserProfile.query.filter_by(username=username).first()
+            if user is not None and check_password_hash(user.password, password):
 
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
@@ -44,10 +50,16 @@ def login():
             # passed to the login_user() method below.
 
             # get user id, load into session
-            login_user(user)
+                login_user(user)
+                flash('Logged in successfully.', 'success')
+                return redirect(url_for('secure_page'))
+            else:
+                flash('Username or Password is incorrect.', 'danger')
+
 
             # remember to flash a message to the user
             return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+    flash_errors(form)
     return render_template("login.html", form=form)
 
 
